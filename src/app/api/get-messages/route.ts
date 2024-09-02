0bee61c9a38e5afe780f1915c22bb78a38836b01
4bh16am <45392298+abhi123vj@users.noreply.github.com>
2024-09-02 12:11:15 +0530

    reset

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -18,22 +18,22 @@ export async function GET(request: Request) {
   }
   const userId = new mongoose.Types.ObjectId(_user._id);
   try {
-    const user = await UserModel.aggregate([
+    const messages = await UserModel.aggregate([
       { $match: { _id: userId } },
       { $unwind: '$messages' },
       { $sort: { 'messages.createdAt': -1 } },
       { $group: { _id: '$_id', messages: { $push: '$messages' } } },
     ]).exec();
 
-    if (!user || user.length === 0) {
+    if (!messages || messages.length === 0) {
       return Response.json(
-        { message: 'User not found', success: false },
-        { status: 404 }
+        { message: 'No New Messages', success: true },
+        { status: 200 }
       );
     }
 
     return Response.json(
-      { messages: user[0].messages },
+      { messages: messages[0].messages },
       {
         status: 200,
       }
